c8db450f57ea42968c002a82206ca290a8fa37b6
BuildTools <unconfigured@null.spigotmc.org>
2018-09-06 17:01:56 -0400

    randomized the output of an array

--- src/project1/HelloWorld.java
+++ src/project1/HelloWorld.java
@@ -1,8 +1,14 @@
 package project1;
 
+import java.util.Random;
+
 public class HelloWorld {
 
+	private static String[] list = {"How are you?", "Have a nice day!", "How was your weekend?"};
+	
 	public static void main(String[] args) {
+		int i = new Random().nextInt(list.length);
 		System.out.println("Hello World");
+		System.out.println(list[i]);
 	}
 }
