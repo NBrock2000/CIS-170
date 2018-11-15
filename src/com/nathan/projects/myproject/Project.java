a980054ff0e43f5032c6aa17dd631d091fbc2a82
Nathan Brock <sNathanBrock@yccc.edu>
2018-11-14 19:18:34 -0500

    added files for upcoming projects

---
+++ src/com/nathan/projects/myproject/Project.java
@@ -0,0 +1,16 @@
+package com.nathan.projects.myproject;
+
+public class Project {
+
+	/* Nathan Brock
+	 * 
+	 * Definition: 
+	 * - http://celestialcoding.com/java/150-java-program-ideas/
+	 * 
+	 * Criteria:
+	 * - 
+	 * - 
+	 * - 
+	 * - 
+	 */
+}
