2ec550ea89f41430a81f68f8928250be116f0578
BuildTools <unconfigured@null.spigotmc.org>
2018-11-28 14:08:31 -0500

    progress made

--- src/com/nathan/projects/user_modeling/Address.java
+++ src/com/nathan/projects/user_modeling/Address.java
@@ -1,5 +1,54 @@
 package com.nathan.projects.user_modeling;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Address {
 
+	private String street;
+	private String city;
+	private String state;
+	private String zipcode;
+	
+	List<String> address = new ArrayList<String>();
+	public void addAddress() {
+		if(address.isEmpty()) {
+			address.add(street);
+			address.add(city);
+			address.add(state);
+			address.add(zipcode);
+		} else {
+			for(int i = 0; i < address.size(); i++) {
+				address.remove(i);
+			}
+			address.add(street);
+			address.add(city);
+			address.add(state);
+			address.add(zipcode);
+		}
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
 }
